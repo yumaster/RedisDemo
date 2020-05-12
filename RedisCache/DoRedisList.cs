b09dd9492eca4868989e541346710a7ebab268f8
yumaster <yumaster@yeah.net>
2020-05-12 09:42:27 +0800

    add do redislist 阻塞命令

--- RedisCache/DoRedisList.cs
+++ RedisCache/DoRedisList.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ServiceStack.Redis;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -135,6 +136,96 @@ namespace RedisCache
         {
             Core.AddRangeToList(key, values);
             Core.ExpireEntryIn(key, sp);
+        }
+        #endregion
+
+        #region 获取值
+        /// <summary>
+        /// 获取list中key包含的数据数量
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public long Count(string key)
+        {
+            return Core.GetListCount(key);
+        }
+        /// <summary>
+        /// 获取key包含的所有数据集合
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public List<string>Get(string key)
+        {
+            return Core.GetAllItemsFromList(key);
+        }
+        /// <summary>
+        /// 获取key中下标为star到end的值集合
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="star"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
+        public List<string>Get(string key,int star,int end)
+        {
+            return Core.GetRangeFromList(key, star, end);
+        }
+        #endregion
+
+        #region 阻塞命令
+        /// <summary>
+        /// 阻塞命令：从list中key的尾部移除一个值，并返回移除的值，阻塞时间为sp
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="sp"></param>
+        /// <returns></returns>
+        public string BlockingPopItemFromList(string key,TimeSpan? sp)
+        {
+            return Core.BlockingPopItemFromList(key, sp);
+        }
+        /// <summary>
+        /// 阻塞命令：从list中keys的尾部移除一个值，并返回移除的值，阻塞时间为sp
+        /// </summary>
+        /// <param name="keys"></param>
+        /// <param name="sp"></param>
+        /// <returns></returns>
+        public ItemRef BlockingPopItemFromLists(string[]keys,TimeSpan?sp)
+        {
+            return Core.BlockingPopItemFromLists(keys, sp);
+        }
+        /// <summary>
+        ///  阻塞命令：从list中keys的尾部移除一个值，并返回移除的值，阻塞时间为sp
+        /// </summary>  
+        public string BlockingDequeueItemFromList(string key, TimeSpan? sp)
+        {
+            return Core.BlockingDequeueItemFromList(key, sp);
+        }
+        /// <summary>
+        /// 阻塞命令：从list中keys的尾部移除一个值，并返回移除的值，阻塞时间为sp
+        /// </summary>  
+        public ItemRef BlockingDequeueItemFromLists(string[] keys, TimeSpan? sp)
+        {
+            return Core.BlockingDequeueItemFromLists(keys, sp);
+        }
+        /// <summary>
+        /// 阻塞命令：从list中key的头部移除一个值，并返回移除的值，阻塞时间为sp
+        /// </summary>  
+        public string BlockingRemoveStartFromList(string keys, TimeSpan? sp)
+        {
+            return RedisBase.Core.BlockingRemoveStartFromList(keys, sp);
+        }
+        /// <summary>
+        /// 阻塞命令：从list中key的头部移除一个值，并返回移除的值，阻塞时间为sp
+        /// </summary>  
+        public ItemRef BlockingRemoveStartFromLists(string[] keys, TimeSpan? sp)
+        {
+            return RedisBase.Core.BlockingRemoveStartFromLists(keys, sp);
+        }
+        /// <summary>
+        /// 阻塞命令：从list中一个fromkey的尾部移除一个值，添加到另外一个tokey的头部，并返回移除的值，阻塞时间为sp
+        /// </summary>  
+        public string BlockingPopAndPushItemBetweenLists(string fromkey, string tokey, TimeSpan? sp)
+        {
+            return RedisBase.Core.BlockingPopAndPushItemBetweenLists(fromkey, tokey, sp);
         }
         #endregion
 
