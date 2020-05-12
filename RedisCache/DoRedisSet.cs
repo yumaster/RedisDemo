f48daeb634707b688f9e6866689a43d485fe2eb7
yumaster <yumaster@yeah.net>
2020-05-12 09:54:03 +0800

    DoRedisSet

---
+++ RedisCache/DoRedisSet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisCache
+{
+    /// <summary>
+    /// .Net操作Redis数据类型Set
+    /// </summary>
+    public class DoRedisSet:DoRedisBase
+    {
+    }
+}
