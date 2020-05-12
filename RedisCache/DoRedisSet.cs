e1a9fa9f20d5643d77d72973e77f716cd8b72c35
yumaster <yumaster@yeah.net>
2020-05-12 09:56:17 +0800

    add add

--- RedisCache/DoRedisSet.cs
+++ RedisCache/DoRedisSet.cs
@@ -11,5 +11,25 @@ namespace RedisCache
     /// </summary>
     public class DoRedisSet:DoRedisBase
     {
+        #region 添加
+        /// <summary>
+        /// key集合中添加value值
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void Add(string key,string value)
+        {
+            Core.AddItemToSet(key, value);
+        }
+        /// <summary>
+        /// key集合添加list集合
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="list"></param>
+        public void Add(string key,List<string>list)
+        {
+            Core.AddRangeToSet(key, list);
+        }
+        #endregion
     }
 }
